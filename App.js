b7d58c6d582a09c69126fe900a8195c956264c3d
muhamad-anggraito <muhamad.anggraito@gmail.com>
2020-09-27 14:22:29 +0700

    checkpoint 3

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
+import firebase from 'firebase';
 
+import config from './src/config';
 import Header from './src/components/Header';
+import LoginForm from './src/components/LoginForm';
 
 class App extends Component {
+  componentDidMount() {
+    if (!firebase.apps.length) {
+      console.log('Init Firebase...');
+      firebase.initializeApp(config.firebaseConfig);
+    }
+  }
   render() {
     return (
       <View>
         <Header title="Authentication" />
+
+        <LoginForm />
       </View>
     );
   }
---
+++ src/components/LoginForm.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <Card>
+        <CardSection></CardSection>
+        <CardSection></CardSection>
+        <CardSection>
+          <Button>Login</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
