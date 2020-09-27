6d88e43dfdf29b88b4559efee70ccdb34c519d1f
muhamad-anggraito <muhamad.anggraito@gmail.com>
2020-09-27 15:00:28 +0700

    checkpoint 4

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,13 +3,22 @@ import React, {Component} from 'react';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
+import Input from './Input'
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
-        <CardSection></CardSection>
-        <CardSection></CardSection>
+        <CardSection>
+          <Input label="Email" placeholder="user@gmail.com" />
+        </CardSection>
+        <CardSection>
+          <Input
+            label="Password"
+            placeholder="password"
+            secureTextEntry={true}
+          />
+        </CardSection>
         <CardSection>
           <Button>Login</Button>
         </CardSection>
