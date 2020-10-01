585037536148b2599bdc12278b53b5cd7049938e
muhamad-anggraito <muhamad.anggraito@gmail.com>
2020-10-01 19:50:16 +0700

    checkpoint 8

--- App.js
+++ App.js
@@ -5,12 +5,46 @@ import firebase from 'firebase';
 import config from './src/config';
 import Header from './src/components/Header';
 import LoginForm from './src/components/LoginForm';
+import CardSection from './src/components/CardSection';
+import Card from './src/components/Card';
+import Button from './src/components/Button';
+import Spinner from './src/components/Spinner';
 
 class App extends Component {
+  state = {
+    loggedIn: null,
+  };
   componentDidMount() {
     if (!firebase.apps.length) {
       console.log('Init Firebase...');
       firebase.initializeApp(config.firebaseConfig);
+    }
+
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setState({loggedIn: !!user});
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Card>
+            <CardSection>
+              <Button>Logout</Button>
+            </CardSection>
+          </Card>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return (
+          <Card>
+            <CardSection>
+              <Spinner />
+            </CardSection>
+          </Card>
+        );
     }
   }
   render() {
@@ -18,7 +52,7 @@ class App extends Component {
       <View>
         <Header title="Authentication" />
 
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
